ae1851358105a81dba2a7430bc8dbbfbc186b9f0
ubaydillah1 <ubaydillah1737@gmail.com>
2025-01-14 02:35:31 +0700

    create views, adding some html, and adding styles

--- Create Server Express/app.js
+++ Create Server Express/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const path = require("path");
 
 const app = express();
 
@@ -10,7 +11,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000, () => {
--- Create Server Express/routes/admin.js
+++ Create Server Express/routes/admin.js
@@ -1,11 +1,11 @@
 const express = require("express");
+const path = require("path");
+const rootDir = require("../utils/path");
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    "<h1>Add Product Page</h1><form action='/admin/add-product' method='POST'><input type='text' name='title'/> <button type='submit'>Add Product</button></form>"
-  );
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {

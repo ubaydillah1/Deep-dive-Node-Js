d93babc7117892a5b2870fa913de271d4153241d
ubaydillah1 <ubaydillah1737@gmail.com>
2025-01-14 01:35:01 +0700

    end routes

--- Create Server Express/app.js
+++ Create Server Express/app.js
@@ -6,8 +6,12 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
 
 app.listen(3000, () => {
   console.log("Server runs on http://localhost:3000 ");
